ca6d6e32d2844fc5416f5667c73a194a39dfc61a
gabriel cordoba <gabrielcordoba.gc@gmail.com>
2024-10-30 00:38:39 -0300

    primer commit, traje todo de servicio

--- ApiTpEncode/obj/Debug/net8.0/ApiTpEncode.AssemblyInfo.cs
+++ ApiTpEncode/obj/Debug/net8.0/ApiTpEncode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiTpEncode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f95b981ac3fbd4a50fe272372d3cfb2add8db9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e73751429b7c17460e02ad83fc1e9dba3840e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiTpEncode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiTpEncode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
